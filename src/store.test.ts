4bc968acdff09adb313327f860689512d1e1a289
Kirill Enykeev <enykeev@mech.sh>
2023-08-08 07:30:01 +0200

    add basic store

---
+++ src/store.test.ts
@@ -0,0 +1,115 @@
+import { expect, test, describe, beforeEach, setSystemTime } from "bun:test";
+
+import * as store from "./store";
+
+const ISO8601 = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
+
+function advanceTime(ms: number) {
+  const now = new Date();
+  now.setMilliseconds(now.getMilliseconds() + ms);
+  setSystemTime(now);
+}
+
+describe("store", () => {
+  beforeEach(() => {
+    setSystemTime();
+    store.db.exec("DELETE FROM flags");
+    store.createFlag({
+      appId: "test",
+      flagId: "test",
+      name: "test",
+      description: "test",
+      value: true,
+    });
+  });
+
+  test("listFlags", () => {
+    const flags = store.listFlags({ appId: "test" });
+    expect(flags).toEqual([
+      {
+        appId: "test",
+        flagId: "test",
+        name: "test",
+        description: "test",
+        createdAt: expect.stringMatching(ISO8601),
+        updatedAt: expect.stringMatching(ISO8601),
+        value: true,
+      },
+    ]);
+  });
+
+  test("getFlag", () => {
+    const flag = store.getFlag({ appId: "test", flagId: "test" });
+    expect(flag).toEqual({
+      appId: "test",
+      flagId: "test",
+      name: "test",
+      description: "test",
+      createdAt: expect.stringMatching(ISO8601),
+      updatedAt: expect.stringMatching(ISO8601),
+      value: true,
+    });
+  });
+
+  test("createFlag", () => {
+    store.createFlag({
+      appId: "test",
+      flagId: "test2",
+      name: "test2",
+      description: "test2",
+      value: false,
+    });
+    const flag = store.getFlag({ appId: "test", flagId: "test2" });
+    expect(flag).toEqual({
+      appId: "test",
+      flagId: "test2",
+      name: "test2",
+      description: "test2",
+      createdAt: expect.stringMatching(ISO8601),
+      updatedAt: expect.stringMatching(ISO8601),
+      value: false,
+    });
+  });
+
+  test("updateFlag", () => {
+    advanceTime(1);
+    store.updateFlag({
+      appId: "test",
+      flagId: "test",
+      name: "test2",
+      description: "test2",
+      value: false,
+    });
+    const flag = store.getFlag({ appId: "test", flagId: "test" });
+    expect(flag).toEqual({
+      appId: "test",
+      flagId: "test",
+      name: "test2",
+      description: "test2",
+      createdAt: expect.stringMatching(ISO8601),
+      updatedAt: expect.stringMatching(ISO8601),
+      value: false,
+    });
+    expect(flag?.updatedAt).not.toEqual(flag?.createdAt);
+  });
+
+  test("deleteFlag", () => {
+    store.deleteFlag({ appId: "test", flagId: "test" });
+    const flag = store.getFlag({ appId: "test", flagId: "test" });
+    expect(flag).toEqual(null);
+  });
+
+  test("getFlagValue", () => {
+    const value = store.getFlagValue({ appId: "test", flagId: "test" });
+    expect(value).toEqual({ value: true });
+  });
+
+  test("setFlagValue", () => {
+    advanceTime(1);
+    store.setFlagValue({ appId: "test", flagId: "test", value: false });
+    const value = store.getFlagValue({ appId: "test", flagId: "test" });
+    expect(value).toEqual({ value: false });
+    const flag = store.getFlag({ appId: "test", flagId: "test" });
+    expect(flag?.updatedAt).not.toEqual(flag?.createdAt);
+  });
+});
---
+++ src/store.ts
@@ -0,0 +1,182 @@
+import { Database } from "bun:sqlite";
+
+export const db = new Database();
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS flags (
+    appId TEXT NOT NULL,
+    flagId TEXT NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT,
+    createdAt TEXT NOT NULL,
+    updatedAt TEXT NOT NULL,
+    meta TEXT NOT NULL,
+    PRIMARY KEY (appId, flagId)
+  )
+`);
+
+export type FlagDB = {
+  appId: string;
+  flagId: string;
+  name: string;
+  description: string | null;
+  createdAt: string;
+  updatedAt: string;
+  meta: string;
+};
+
+export const queries = {
+  listFlags: db.prepare<FlagDB, [FlagDB["appId"]]>(
+    "SELECT * FROM flags where appId = ?",
+  ),
+  getFlag: db.prepare<FlagDB, [FlagDB["appId"], FlagDB["flagId"]]>(
+    "SELECT * FROM flags where appId = ? and flagId = ?",
+  ),
+  createFlag: db.prepare<
+    FlagDB,
+    [
+      FlagDB["appId"],
+      FlagDB["flagId"],
+      FlagDB["name"],
+      FlagDB["description"],
+      FlagDB["createdAt"],
+      FlagDB["updatedAt"],
+      FlagDB["meta"],
+    ]
+  >(
+    "INSERT INTO flags (appId, flagId, name, description, createdAt, updatedAt, meta) VALUES (?, ?, ?, ?, ?, ?, ?)",
+  ),
+  updateFlag: db.prepare<
+    FlagDB,
+    [
+      FlagDB["name"],
+      FlagDB["description"],
+      FlagDB["updatedAt"],
+      FlagDB["meta"],
+      FlagDB["appId"],
+      FlagDB["flagId"],
+    ]
+  >(
+    "UPDATE flags SET name = ?, description = ?, updatedAt = ?, meta = ? WHERE appId = ? and flagId = ?",
+  ),
+  deleteFlag: db.prepare<FlagDB, [FlagDB["appId"], FlagDB["flagId"]]>(
+    "DELETE FROM flags WHERE appId = ? and flagId = ?",
+  ),
+};
+
+export type Flag = Omit<FlagDB, "meta"> & Record<string, unknown>;
+
+export type ListFlagArgs = Pick<FlagDB, "appId">;
+export type ListFlagResult = Flag[];
+
+export function listFlags({ appId }: ListFlagArgs): ListFlagResult {
+  return queries.listFlags.all(appId).map((flag) => {
+    const { meta, ...rest } = flag;
+    return { ...rest, ...JSON.parse(meta) };
+  });
+}
+
+export type GetFlagArgs = Pick<FlagDB, "appId" | "flagId">;
+export type GetFlagResult = Flag | null;
+
+export function getFlag({ appId, flagId }: GetFlagArgs): GetFlagResult {
+  const res = queries.getFlag.get(appId, flagId);
+  if (!res) {
+    return null;
+  }
+
+  const { meta, ...rest } = res;
+
+  return { ...rest, ...JSON.parse(meta) };
+}
+
+export type CreateFlagArgs = Omit<FlagDB, "meta" | "createdAt" | "updatedAt"> &
+  Record<string, unknown>;
+
+export function createFlag({
+  appId,
+  flagId,
+  name,
+  description,
+  ...rest
+}: CreateFlagArgs) {
+  const meta = JSON.stringify(rest);
+  const updatedAt = new Date().toISOString();
+  const createdAt = updatedAt;
+  return queries.createFlag.run(
+    appId,
+    flagId,
+    name,
+    description || null,
+    createdAt,
+    updatedAt,
+    meta,
+  );
+}
+
+export type updateFlagArgs = Omit<FlagDB, "meta" | "createdAt" | "updatedAt"> &
+  Record<string, unknown>;
+
+export function updateFlag({
+  appId,
+  flagId,
+  name,
+  description,
+  ...rest
+}: updateFlagArgs) {
+  const meta = JSON.stringify(rest);
+  const updatedAt = new Date().toISOString();
+  return queries.updateFlag.run(
+    name,
+    description || null,
+    updatedAt,
+    meta,
+    appId,
+    flagId,
+  );
+}
+
+export type DeleteFlagArgs = Pick<FlagDB, "appId" | "flagId">;
+
+export function deleteFlag({ appId, flagId }: DeleteFlagArgs) {
+  return queries.deleteFlag.run(appId, flagId);
+}
+
+export type GetFlagValueArgs = Pick<FlagDB, "appId" | "flagId">;
+export type GetFlagValueResult = Record<string, unknown> | null;
+
+export function getFlagValue({
+  appId,
+  flagId,
+}: GetFlagValueArgs): GetFlagValueResult {
+  const flag = queries.getFlag.get(appId, flagId);
+  if (!flag) {
+    return null;
+  }
+  return JSON.parse(flag.meta);
+}
+
+export type SetFlagValueArgs = Pick<FlagDB, "appId" | "flagId"> &
+  Record<string, unknown>;
+
+export function setFlagValue({
+  appId,
+  flagId,
+  ...rest
+}: SetFlagValueArgs): GetFlagValueResult {
+  const flag = queries.getFlag.get(appId, flagId);
+  if (!flag) {
+    return null;
+  }
+  const meta = JSON.stringify(rest);
+  const updatedAt = new Date().toISOString();
+  queries.updateFlag.run(
+    flag.name,
+    flag.description,
+    updatedAt,
+    meta,
+    appId,
+    flagId,
+  );
+  return rest;
+}
