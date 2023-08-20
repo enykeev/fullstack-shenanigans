9a0b75be30a1e4de19893677e30be49717a519a2
Kirill Enykeev <enykeev@mech.sh>
2023-08-20 11:33:34 +0200

    add mermaid printer

--- packages/filter-query/debugger.ts
+++ packages/filter-query/debugger.ts
@@ -5,7 +5,7 @@ export function printAST(node: Node | undefined, level = 0) {
     return;
   }
 
-  console.log("  ".repeat(level), node.type, node?.subtype || "");
+  console.log("  ".repeat(level), node.type, node.subtype || "");
 
   switch (node.type) {
     case "accessor":
@@ -29,4 +29,66 @@ export function printAST(node: Node | undefined, level = 0) {
       break;
     }
   }
+}
+
+function printASTMermaidNode(
+  ctx: { i: number },
+  node: Node | undefined,
+  from: number | string,
+) {
+  if (!node) {
+    return;
+  }
+
+  const to = ctx.i++;
+
+  const subtype = node.subtype?.replace("&&", "and").replace("||", "or");
+
+  console.log(
+    "  ",
+    from,
+    "-->",
+    subtype ? `${to}[${node.type} ${subtype}]` : `${to}[${node.type}]`,
+  );
+
+  switch (node.type) {
+    case "accessor":
+    case "value": {
+      break;
+    }
+    case "boolean":
+    case "comparison": {
+      printASTMermaidNode(ctx, node.left, to);
+      printASTMermaidNode(ctx, node.right, to);
+      break;
+    }
+    case "group": {
+      printASTMermaidNode(ctx, node.value, to);
+      break;
+    }
+    case "list": {
+      for (const child of node.children) {
+        printASTMermaidNode(ctx, child, to);
+      }
+      break;
+    }
+  }
+}
+
+export function printASTMermaid(node: Node | undefined, title?: string) {
+  if (!node) {
+    return;
+  }
+
+  if (title) {
+    console.log(
+      `---\ntitle: ${title
+        .replaceAll("&", "#amp;")
+        .replaceAll(">", "#gt;")
+        .replaceAll("<", "#lt;")} \n---`,
+    );
+  }
+
+  console.log("flowchart TD");
+  printASTMermaidNode({ i: 0 }, node, "start");
 }
--- packages/filter-query/index.ts
+++ packages/filter-query/index.ts
@@ -1,13 +1,11 @@
-import { printAST } from "./debugger";
+import { printASTMermaid } from "./debugger";
 import { parse } from "./parser";
 
 const query = `aaa.bb.c == 1.5 && (res.some !== "thing" or FALSE) && value >= -1 && value < -.16 and key in [1,'2',3.0]`;
-console.log(">", query);
-
 const ast = parse(query);
 // console.log(ast);
 
-printAST(ast);
+printASTMermaid(ast, query);
 
 // ast = parse(`some == 1.5 && value !== -1`);
 
